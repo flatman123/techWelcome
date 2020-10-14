d586886bac8e63bb08340a6406114ec47911e19a
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-10-14 09:59:05 -0400

    added middleware for protected routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,15 +10,12 @@ module.exports = function(req, res, next) {
     if(!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
-
     // Verify token
     try {
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
-
-        console.log(`your DECODED INFORMATION JEFF! ${decoded}`)
+        const decoded = jwt.verify(token,config.get('jwtSecret'));
         req.user = decoded.user;
         next();
-    }   catch(err){
+    } catch(err){
         res.status(401).json({ msg: 'Token is not valid' });
     }
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,22 @@
+const { json } = require('express');
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const user = require('../../models/User');
 
 // @route GET api/auth
 // @desc  auth Route
 // @access Public
-router.get('/', (req, res) => res.send('Auth Router'));
+
+// Note adding 'auth' in the route below will make protected!
+router.get('/',auth, async (req, res) => {
+    try {
+        const userData = await user.findById(req.user.id);
+        res.json(userData);
+    } catch(err) {
+        console.error(err.messages);
+        res.status(500).send('Server Error');
+    };
+});
 
 module.exports = router;
