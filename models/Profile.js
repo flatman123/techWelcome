4d06283bcb540b8d235220ae2aaf0433444fb8bb
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-10-23 00:02:27 -0400

    nothing changed

--- models/Profile.js
+++ models/Profile.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = mongoose.Schema({
-    user: {
+    userPro: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     },
