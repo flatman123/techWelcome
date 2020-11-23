2a9a6c0295b588a2990a5995ee47bfa0a3c67427
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-11-23 00:02:20 -0500

    completed router for creating post, added route for retrieving all posts; a private route

--- routes/api/post.js
+++ routes/api/post.js
@@ -26,17 +26,32 @@ router.post('/', [
     try {
         const user = await User.findById(req.user.id).select('-password');
 
-        const newPost = {
+        const newPost = new Post({
             text: req.body.text,
             name: user.name,
             avatar: user.avatar,
             user: req.user.id
-        };
+        });
+        const post = await newPost.save();
+        res.json(post);
+
     } catch (err) {
         console.error(err.message);
         return res.status(500).send('Server Error');
     };
 
+});
+
+// @route GET api/posts
+// @desc Get all posts
+// @access Private
+router.get('/', auth, async(req, res)=> {
+    try{
+        const posts = await Post.find({}).sort({ date: -1 });
+    } catch(err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+    };
 });
 
 module.exports = router;
