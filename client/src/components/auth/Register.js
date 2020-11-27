2025e3afc1ff0daa453ea0c11ed45f91ffec4a0f
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-11-27 11:08:44 -0500

    completed Register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
-
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     const [ formData, setFormData ] = useState({
@@ -10,14 +11,16 @@ const Register = () => {
     });
     const { name, email, password, password2 } = formData;
     const onChange = e => setFormData( {...formData, [e.target.name]:e.target.value});
-    const onSubmit = e => {
+   
+    const onSubmit = async e => {
         e.preventDefault();
+
         if(password !== password2) {
             console.log('Passwords to not match')
         } else {
-            console.log(formData)
+            console.log('SUCCESS');
         }
-    }
+    };
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
@@ -78,7 +81,7 @@ const Register = () => {
             </form>
 
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </Fragment>
 
