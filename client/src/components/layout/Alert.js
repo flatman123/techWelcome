ddd74dd9b5a2a363879ae5dce1eec722b327ebe9
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-11-30 11:04:53 -0500

    completed Alert component

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+import { STATES } from 'mongoose';
 
-const Alert = props => {
-    return (
-        <div>
+const Alert = props => 
+    props.alerts !== null && 
+    props.alerts.length > 0 && 
+    props.alerts.map(alert => (
 
-        </div>
-    )
-};
+            <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+                { alert.msg }
+            </div>
+));
+
+
 
 Alert.propTypes = {
-
+    alerts: PropTypes.array.isRequired
 };
 
-export default Alert;
+// Grab state for alerts from redux 
+const mapStateToProps = state => {
+    return {
+        alerts: state.alert
+    }
+}
+
+// Grab the current state from REdux and map it to props so other components can use is
+
+export default connect(mapStateToProps)(Alert);
