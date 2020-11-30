391df0837199d38ce7beb195c7a807bd6257a552
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-11-30 13:27:45 -0500

    completed action for user registration

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_FAIL, REGISTER_SUCCESS, REGUSTER_FAIL } from './types';
+import { REGISTER_FAIL, REGISTER_SUCCESS } from './types';
 
 // Register User
+
+// Thunk is used to pass in parameters, while also making an api request.
 export const register = ({ name, email, password }) => async dispatch => {
+    // Assemble headers and body for api request
     const config = {
         headers: {
             'Content-Type': 'application/json'
         }
-    }
+    };
 
     const body = JSON.stringify({ name, email, password });
 
@@ -19,8 +22,14 @@ export const register = ({ name, email, password }) => async dispatch => {
         dispatch({
             type: REGISTER_SUCCESS,
             payload: servResponse.data  
-        })
+        });
     } catch (err) {
+        const errors = err.response.data.errors
+        console.log(err)
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
         dispatch({
             type: REGISTER_FAIL
         });
