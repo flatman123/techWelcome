30994d00a6c8d3a040ff3c28b4fbd9a0a562305f
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-11-30 11:02:49 -0500

    added code for alert action

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,10 +2,25 @@ import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeOut = 3000) => (dispatch, getState) => {
     const id = uuid();
     dispatch({
         type: SET_ALERT,
-        pyload: { msg: aalertType, id }
+        payload: { msg, alertType, id }
     });
+
+    setTimeout(() => {
+        return dispatch({ type: REMOVE_ALERT, payload: id })
+    }, timeOut)
 };
+
+
+// export function setAlert() {
+//     return function(dispatch) {
+//         const id = uuid();
+//         return dispatch({
+    //     type: SET_ALERT,
+    //     payload: { msg: aalertType, id }
+    // });
+//     }
+// }
