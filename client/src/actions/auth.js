aa5d5c0f8b9d78e24bd797aa43ba4b45aa2bcf20
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-01 08:41:22 -0500

    finished code for loading Usr

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,9 +1,32 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_FAIL, REGISTER_SUCCESS } from './types';
+import { REGISTER_FAIL, REGISTER_SUCCESS, USER_LOADED, AUTH_ERROR } from './types';
+import { setAuthToken } from '../utils/setAuthToken';
+
+
+// Load User ( Login in User)
+export const loadUser = () => async dispatch => {
+    if (localStorage.token) {
+        setAuthToken(localStorage.token);
+    };
+
+    try {
+        const servResponse = await axios.get('/api/auth');
+
+        dispatch({
+            type: USER_LOADED,
+            payload: servResponse.data
+        })
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR
+        });
+    }
+};
+
+
 
 // Register User
-
 // Thunk is used to pass in parameters, while also making an api request.
 export const register = ({ name, email, password }) => async dispatch => {
     // Assemble headers and body for api request
