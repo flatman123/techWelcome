02e720b6fa1001bdddfd06a3b537587899c5f717
Jeffrey McIntyre <redeyecoding@gmail.com>
2020-12-01 08:40:58 -0500

    added function setAuthToken to code, added useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -6,12 +6,25 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import './App.css';
+import { setAuthToken } from './utils/setAuthToken';
 
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+
+
+// First check to see if the current user is logged in.
+// Run on each load & Re-load of the application
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+};
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser)
+  }, []);
+
   return (
     <Provider store={store}>
      <Router>
